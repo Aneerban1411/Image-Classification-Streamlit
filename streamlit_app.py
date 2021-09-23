ccd6a554fad57faaadf1435ea9634d7a99eafcc8
Aneerban Chakraborty <aneerbanchakraborty@Aneerbans-MacBook-Pro.local>
2021-09-23 13:18:37 +0530

    URL and output format change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,22 @@
+from requests.models import MissingSchema
 import streamlit as st
 import cv2
 import numpy as np
+from PIL import Image, UnidentifiedImageError
+import requests
+from io import BytesIO
+
 
 # Create application title and file uploader widget.
 st.title("OpenCV Deep Learning based Image Classification")
-img_file_buffer = st.file_uploader("Choose a file", type=['jpg', 'jpeg', 'png'])
+mode = st.selectbox('Upload image from Disc or Web?', ('None', 'Disc', 'Web'))
 
 
 # Function for detecting facses in an image.
 def classify(model, image, class_names):
+
+    # Converting RGB image to BGR OpenCV image
+    image = np.uint8(image)[:,:,::-1]
     
     # create blob from image
     blob = cv2.dnn.blobFromImage(image=image, scalefactor=0.01, size=(224, 224), 
@@ -29,8 +37,12 @@ def classify(model, image, class_names):
     final_prob = np.max(probs) * 100.
     # map the max confidence to the class label names
     out_name = class_names[label_id]
-    out_text = f"Class: {out_name}, Confidence: {final_prob:.3f}"
+    out_text = f"Class: {out_name}, Confidence: {final_prob:.1f}%"
     return out_text
+
+def header(text):
+    st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;" align="center">{text}</p>', unsafe_allow_html=True)
+
 
 
 
@@ -50,17 +62,33 @@ def load_model():
                         framework='Caffe')
     return model, class_names
 
-
 net, class_names = load_model()
 
-if img_file_buffer is not None:
-    # Read the file and convert it to opencv Image.
-    raw_bytes = np.asarray(bytearray(img_file_buffer.read()), dtype=np.uint8)
-    # Loads image in a BGR channel order.
-    image = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
-    st.image(image[:,:,::-1])
+if mode == 'Disc':
+    img_file_buffer = st.file_uploader("Choose a file", type=['jpg', 'jpeg', 'png'])
+    if img_file_buffer is not None:
+        # Read the file and convert it to opencv Image.
+        image = np.array(Image.open(img_file_buffer))
+        st.image(image)
 
-    # Call the classification model to detect faces in the image.
-    detections = classify(net, image, class_names)
+        # Call the classification model to detect faces in the image.
+        detections = classify(net, image, class_names)
 
-    st.header(detections)
+        header(detections)
+elif mode == 'Web':
+    url = st.text_input('Enter URL')
+
+    if url != '':
+        try:
+            response = requests.get(url)
+            image = Image.open(BytesIO(response.content))
+            st.image(image)
+
+            # Call the classification model to detect faces in the image.
+            detections = classify(net, image, class_names)
+
+            header(detections)
+        except MissingSchema:
+            st.header('Invalid URL, Try Again!')
+        except UnidentifiedImageError:
+            st.header('URL has no Image, Try Again!')
